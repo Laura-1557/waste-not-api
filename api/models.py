58cea6616eed64374e857945eec3295122ef0a1f
Laura-1557 <94494994+Laura-1557@users.noreply.github.com>
2022-05-07 20:55:15 +0100

    Creation of models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
+from ast import FloorDiv
+import code
+from pyexpat import model
 from django.db import models
+import string
+import random
+
+def generate_unique_order_number():
+    length = 30
+
+    while True:
+        food_pack_id = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Order.objects.filter(food_pack_id=code).count() == 0:
+            break
+    return food_pack_id
 
 # Create your models here.
+class Order(models.Model):
+    food_pack_id = models.CharField(max_length=8, default="", unique=True)
+    user_id = models.CharField(max_length=50, unique=True)
+    user_chooses_pack = models.BooleanField(null=False, default=False)
+    confirmation_of_choice = models.BooleanField(null=False, default=False)
+    number_of_packs_selected = models.IntegerField(null=False, default=0)
+    order_placed_at = models.DateTimeField(auto_now_add=True)
+
+
